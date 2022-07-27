06b657dec7cc0939b6242ffc5595c4aee5594a71
Carl Orge Retzlaff <e.awerg@web.de>
2022-07-27 16:51:57 +0200

    First layout

---
+++ wp-app/wp-content/plugins/woocommerce-gateway-stripe/build/upe_settings.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wc-currency', 'wc-navigation', 'wc-store-data', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-data-controls', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '6b4791ceae5bc045ae7684f8bbe4fda0');
